e8b831c9a164818799604e0711954c73c456f7de
Success <okpuniyi@gmail.com>
2026-08-28 10:35:27 +0100

    Create Question-1/question1.py

---
+++ Question-1/question1.py
@@ -0,0 +1,39 @@
+# NUMERICAL COMPUTATION ASSIGNMENT
+# QUESTION 1
+
+# PART A
+print("PART A: Applications of Numerical Computation")
+print("1. Weather forecasting")
+print("2. Engineering design and analysis")
+print("3. Financial modeling")
+print("4. Computer graphics")
+print("5. Scientific simulations")
+print("6. Medical and biological modeling")
+
+
+# PART B
+print("\nPART B: Substitution Method")
+
+# From equation (1):
+# x + y + z = 6
+# Therefore:
+# x = 6 - y - z
+
+# Substitute x into equation (2):
+# 2x - y + z = 5
+
+# Substitute x into equation (3):
+# x + 2y - z = 3
+
+# Solving the resulting equations:
+# y = 11/7
+# z = 16/7
+# x = 15/7
+
+x = 15 / 7
+y = 11 / 7
+z = 16 / 7
+
+print("x =", round(x, 4))
+print("y =", round(y, 4))
+print("z =", round(z, 4))
